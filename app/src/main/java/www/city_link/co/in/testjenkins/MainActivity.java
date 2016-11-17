5212d1409909fc855bf0543aa3964338337d476f
Kiran Kumar D <dkiran@live.com>
2016-11-17 18:45:31 +0530

    First Commit

---
+++ app/src/main/java/www/city_link/co/in/testjenkins/MainActivity.java
@@ -0,0 +1,13 @@
+package www.city_link.co.in.testjenkins;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
